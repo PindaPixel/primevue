5302cd5036396b2eb81f81e16ad25105dd36acb1
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2023-12-08 16:09:59 +0300

    Accordion added, except for --p-dark-hover-bg

--- components/lib/theme/lara/accordion/index.js
+++ components/lib/theme/lara/accordion/index.js
@@ -1,9 +1,9 @@
 export default `
 .p-accordion .p-accordion-header .p-accordion-header-link {
     padding: 1.25rem;
-    border: 1px solid #e5e7eb;
-    color: #6b7280;
-    background: #f9fafb;
+    border: 1px solid var(--p-dark-shade-600, var(--p-shade-300));
+    color: var(--p-dark-shade-0, var(--p-shade-600));
+    background: var(--p-dark-shade-800, var(--p-shade-100));
     font-weight: 700;
     border-radius: 6px;
     transition: box-shadow 0.2s;
@@ -14,30 +14,30 @@ export default `
 .p-accordion .p-accordion-header:not(.p-disabled) .p-accordion-header-link:focus-visible {
     outline: 0 none;
     outline-offset: 0;
-    box-shadow: inset 0 0 0 0.2rem #a7f3d0;
+    box-shadow: inset 0 0 0 0.2rem var(--p-primary-lightest-color, var(--p-primary-light-color));
 }
 .p-accordion .p-accordion-header:not(.p-highlight):not(.p-disabled):hover .p-accordion-header-link {
-    background: #f3f4f6;
-    border-color: #e5e7eb;
-    color: #374151;
+    background: var(--p-dark-hover-bg, var(--p-shade-200));
+    border-color: var(--p-dark-shade-600, var(--p-shade-300));
+    color: var(--p-dark-shade-0, var(--p-shade-800));
 }
 .p-accordion .p-accordion-header:not(.p-disabled).p-highlight .p-accordion-header-link {
-    background: #f9fafb;
-    border-color: #e5e7eb;
-    color: #374151;
+    background: var(--p-dark-shade-800, var(--p-shade-100));
+    border-color: var(--p-dark-shade-600, var(--p-shade-300));
+    color: var(--p-dark-shade-0, var(--p-shade-800));
     border-bottom-right-radius: 0;
     border-bottom-left-radius: 0;
 }
 .p-accordion .p-accordion-header:not(.p-disabled).p-highlight:hover .p-accordion-header-link {
-    border-color: #e5e7eb;
-    background: #f3f4f6;
-    color: #374151;
+    border-color: var(--p-dark-shade-600, var(--p-shade-300));
+    background: var(--p-dark-hover-bg, var(--p-shade-200));
+    color: var(--p-dark-shade-0, var(--p-shade-800));
 }
 .p-accordion .p-accordion-content {
     padding: 1.25rem;
-    border: 1px solid #e5e7eb;
-    background: #ffffff;
-    color: #4b5563;
+    border: 1px solid var(--p-dark-shade-300, var(--p-shade-300));
+    background: var(--p-dark-shade-0, var(--p-shade-0));
+    color: var(--p-dark-shade-700, var(--p-shade-700));
     border-top: 0;
     border-top-right-radius: 0;
     border-top-left-radius: 0;
