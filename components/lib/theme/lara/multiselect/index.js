276efe28f6c1900d1d87b80e158a2ab68026bd83
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2023-12-25 13:56:32 +0300

    Form components part III added

--- components/lib/theme/lara/multiselect/index.js
+++ components/lib/theme/lara/multiselect/index.js
@@ -1,108 +1,108 @@
 export default `
 .p-multiselect {
-    background: var(--p-input-bg);
-    border: var(--p-input-border);
-    transition: var(--p-form-element-transition);
-    border-radius: var(--p-border-radius);
+    background: var(--p-input-bg, var(--p-dark-pink-900, var(--p-pink-0)));
+    border: var(--p-input-border, 1px solid var(--p-dark-pink-600, var(--p-pink-400)));
+    transition: var(--p-form-element-transition, background-color var(--p-transition-duration), color var(--p-transition-duration), border-color var(--p-transition-duration), box-shadow var(--p-transition-duration));
+    border-radius: var(--p-border-radius, 6px);
 }
 .p-multiselect:not(.p-disabled):hover {
-    border-color: var(--p-input-hover-border-color);
+    border-color: var(--p-input-hover-border-color, var(--p-dark-primary-color, var(--p-primary-color)));
 }
 .p-multiselect:not(.p-disabled).p-focus {
-    outline: var(--p-focus-outline);
-    outline-offset: var(--p-focus-outline-offset);
-    box-shadow: var(--p-focus-shadow);
-    border-color: var(--p-input-focus-border-color);
+    outline: var(--p-focus-outline, 0 none);
+    outline-offset: var(--p-focus-outline-offset, 0);
+    box-shadow: var(--p-focus-shadow, 0 0 0 .2rem var(--p-dark-primary-lightest-color, var(--p-primary-light-color)));
+    border-color: var(--p-input-focus-border-color, var(--p-dark-primary-color, var(--p-primary-color)));
 }
 .p-multiselect .p-multiselect-label {
-    padding: var(--p-input-padding);
-    transition: var(--p-form-element-transition);
+    padding: var(--p-input-padding, .75rem .75rem);
+    transition: var(--p-form-element-transition, background-color var(--p-transition-duration), color var(--p-transition-duration), border-color var(--p-transition-duration), box-shadow var(--p-transition-duration));
 }
 .p-multiselect .p-multiselect-label.p-placeholder {
-    color: var(--p-input-placeholder-text-color);
+    color: var(--p-input-placeholder-text-color, var(--p-dark-pink-100, var(--p-pink-600)));
 }
 .p-multiselect.p-multiselect-chip .p-multiselect-token {
-    padding: "math.div(nth(var(--p-input-padding), 1), 2) nth(var(--p-input-padding), 2)";
-    margin-right: var(--p-inline-spacing);
-    background: var(--p-chip-bg);
-    color: var(--p-chip-text-color);
-    border-radius: var(--p-chip-border-radius);
+    padding: calc(var(--p-input-padding-top, .75rem) / 2) calc(var(--p-input-padding-right, .75rem) / 2); /* //TODO: */
+    margin-right: var(--p-inline-spacing, .5rem);
+    background: var(--p-chip-bg, var(--p-dark-pink-600, var(--p-pink-300))); /* //TODO: */
+    color: var(--p-chip-text-color, var(--p-dark-text-color, var(--p-text-color)));
+    border-radius: var(--p-chip-border-radius, 16px);
 }
 .p-multiselect.p-multiselect-chip .p-multiselect-token .p-multiselect-token-icon {
-    margin-left: var(--p-inline-spacing);
+    margin-left: var(--p-inline-spacing, .5rem);
 }
 .p-multiselect .p-multiselect-trigger {
-    background: transparent;
-    color: var(--p-input-icon-color);
-    width: var(--p-input-group-add-on-min-width);
-    border-top-right-radius: var(--p-border-radius);
-    border-bottom-right-radius: var(--p-border-radius);
+    background: transparent; /* //TODO: */
+    color: var(--p-input-icon-color, var(--p-dark-pink-100, var(--p-pink-600)));
+    width: var(--p-input-group-add-on-min-width, 3rem);
+    border-top-right-radius: var(--p-border-radius, 6px);
+    border-bottom-right-radius: var(--p-border-radius, 6px);
 }
 .p-multiselect.p-invalid.p-component {
-    border-color: var(--p-input-error-border-color);
+    border-color: var(--p-input-error-border-color, var(--p-dark-error-color, var(--p-error-color)));
 }
 
 .p-inputwrapper-filled.p-multiselect.p-multiselect-chip .p-multiselect-label {
-    padding: "math.div(nth(var(--p-input-padding), 1), 2) nth(var(--p-input-padding), 2)";
+    padding: calc(var(--p-input-padding-top, .75rem) / 2) calc(var(--p-input-padding-right, .75rem) / 2); /* //TODO: */
 }
 
 .p-multiselect-panel {
-    background: var(--p-input-overlay-bg);
-    color: var(--p-input-list-text-color);
-    border: var(--p-input-overlay-border);
-    border-radius: var(--p-border-radius);
+    background: var(--p-input-overlay-bg, var(--p-dark-input-list-bg, var(--p-input-list-bg)));
+    color: var(--p-input-list-text-color, var(--p-dark-pink-0, var(--p-pink-700)));
+    border: var(--p-input-overlay-border, var(1px solid var(--p-dark-pink-600), var(0 none)));
+    border-radius: var(--p-border-radius, 6px);
     box-shadow: var(--p-input-overlay-shadow);
 }
 .p-multiselect-panel .p-multiselect-header {
-    padding: var(--p-input-list-header-padding);
-    border-bottom: var(--p-input-list-header-border);
-    color: var(--p-input-list-header-text-color);
-    background: var(--p-input-overlay-header-bg);
-    margin: var(--p-input-list-header-margin);
-    border-top-right-radius: var(--p-border-radius);
-    border-top-left-radius: var(--p-border-radius);
+    padding: var(--p-input-list-header-padding, .75rem 1.25rem);
+    border-bottom: var(--p-input-list-header-border, 1px solid var(--p-dark-pink-600, var(--p-pink-300)));
+    color: var(--p-input-list-header-text-color, var(--p-dark-pink-0, var(--p-pink-800)));
+    background: var(--p-input-overlay-header-bg, var(--p-dark-input-list-header-bg, var(--p-input-list-header-bg)));
+    margin: var(--p-input-list-header-margin, 0);
+    border-top-right-radius: var(--p-border-radius, 6px);
+    border-top-left-radius: var(--p-border-radius, 6px);
 }
 .p-multiselect-panel .p-multiselect-header .p-multiselect-filter-container .p-inputtext {
-    padding-right: "nth(var(--p-input-padding), 2) + var(--p-prime-icon-font-size)";
+    padding-right: calc(var(--p-input-padding-right, .75rem) + var(--p-prime-icon-font-size, 1rem));
 }
 .p-multiselect-panel .p-multiselect-header .p-multiselect-filter-container .p-multiselect-filter-icon {
-    right: "nth(var(--p-input-padding), 2)";
-    color: var(--p-input-icon-color);
+    right: var(--p-input-padding-right, .75rem);
+    color: var(--p-input-icon-color, var(--p-dark-pink-100, var(--p-pink-600)));
 }
 .p-multiselect-panel .p-multiselect-header .p-checkbox {
-    margin-right: var(--p-inline-spacing);
+    margin-right: var(--p-inline-spacing, .5rem);
 }
-.p-multiselect-panel .p-multiselect-header .p-multiselect-close {
-    margin-left: var(--p-inline-spacing);
-    width: var(--p-action-icon-width);
-    height: var(--p-action-icon-height);
+.p-multiselect-panel .p-multiselect-header .p-multiselect-close { /* //TODO: */
+    margin-left: var(--p-inline-spacing, .5rem);
+    width: var(--p-action-icon-width, 2rem);
+    height: var(--p-action-icon-height, 2rem);
     color: var(--p-action-icon-color);
-    border: var(--p-action-icon-border);
-    background: var(--p-action-icon-bg);
-    border-radius: var(--p-action-icon-border-radius);
-    transition: var(--p-action-icon-transition);
+    border: var(--p-action-icon-border, var(--p-dark-pink-100, var(--p-pink-600)));
+    background: var(--p-action-icon-bg, var(--p-dark-pink-900, var(--p-pink-200)));
+    border-radius: var(--p-action-icon-border-radius, 50%);
+    transition: var(--p-action-icon-transition, background-color var(--p-transition-duration), color var(--p-transition-duration), box-shadow var(--p-transition-duration));
 }
 .p-multiselect-panel .p-multiselect-header .p-multiselect-close:enabled:hover {
-    color: var(--p-action-icon-hover-color);
-    border-color: var(--p-action-icon-hover-border-color);
-    background: var(--p-action-icon-hover-bg);
+    color: var(--p-action-icon-hover-color, var(--p-dark-pink-0, var(--p-pink-800)));
+    border-color: var(--p-action-icon-hover-border-color, transparent);
+    background: var(--p-action-icon-hover-bg, var(--p-dark-pink-900, var(--p-pink-200)));
 }
 .p-multiselect-panel .p-multiselect-header .p-multiselect-close:focus-visible {
-    outline: var(--p-focus-outline);
-    outline-offset: var(--p-focus-outline-offset);
-    box-shadow: var(--p-focus-shadow);
+    outline: var(--p-focus-outline, 0 none);
+    outline-offset: var(--p-focus-outline-offset, 0);
+    box-shadow: var(--p-focus-shadow, 0 0 0 .2rem var(--p-dark-primary-lightest-color, var(--p-primary-light-color)));
 }
 .p-multiselect-panel .p-multiselect-items {
-    padding: var(--p-input-list-padding);
+    padding: var(--p-input-list-padding, .75rem 0);
 }
 .p-multiselect-panel .p-multiselect-items .p-multiselect-item {
-    margin: var(--p-input-list-item-margin);
-    padding: var(--p-input-list-item-padding);
-    border: var(--p-input-list-item-border);
-    color: var(--p-input-list-item-text-color);
-    background: var(--p-input-list-item-bg);
-    transition: var(--p-list-item-transition);
-    border-radius: var(--p-input-list-item-border-radius);
+    margin: var(--p-input-list-item-margin, 0);
+    padding: var(--p-input-list-item-padding, .75rem 1.25rem);
+    border: var(--p-input-list-item-border, 0 none);
+    color: var(--p-input-list-item-text-color, var(--p-dark-pink-0, var(--p-pink-700)));
+    background: var(--p-input-list-item-bg, var(--p-dark-pink-800, var(--p-pink-100)));
+    transition: var(--p-list-item-transition, box-shadow var(--p-transition-duration));
+    border-radius: var(--p-input-list-item-border-radius, 0);
 }
 .p-multiselect-panel .p-multiselect-items .p-multiselect-item.p-highlight {
     color: var(--p-highlight-text-color);
@@ -112,36 +112,36 @@ export default `
     background: var(--p-highlight-focus-bg);
 }
 .p-multiselect-panel .p-multiselect-items .p-multiselect-item:not(.p-highlight):not(.p-disabled).p-focus {
-    color: var(--p-input-list-item-text-focus-color);
-    background: var(--p-input-list-item-focus-bg);
+    color: var(--p-input-list-item-text-focus-color, var(--p-dark-pink-0, var(--p-pink-700)));
+    background: var(--p-input-list-item-focus-bg, var(--p-dark-pink-600, var(--p-pink-300)));
 }
 .p-multiselect-panel .p-multiselect-items .p-multiselect-item:not(.p-highlight):not(.p-disabled):hover {
-    color: var(--p-input-list-item-text-hover-color);
-    background: var(--p-input-list-item-hover-bg);
+    color: var(--p-input-list-item-text-hover-color, var(--p-dark-pink-0, var(--p-pink-700)));
+    background: var(--p-input-list-item-hover-bg, var(--p-dark-pink-900, var(--p-pink-200)));
 }
 .p-multiselect-panel .p-multiselect-items .p-multiselect-item .p-checkbox {
-    margin-right: var(--p-inline-spacing);
+    margin-right: var(--p-inline-spacing, .5rem);
 }
-.p-multiselect-panel .p-multiselect-items .p-multiselect-item-group {
-    margin: var(--p-submenu-header-margin);
-    padding: var(--p-submenu-header-padding);
-    color: var(--p-submenu-header-text-color);
-    background: var(--p-submenu-header-bg);
-    font-weight: var(--p-submenu-header-font-weight);
+.p-multiselect-panel .p-multiselect-items .p-multiselect-item-group { /* //TODO: */
+    margin: var(--p-submenu-header-margin, 0);
+    padding: var(--p-submenu-header-padding, .75rem 1.25rem);
+    color: var(--p-submenu-header-text-color, var(--p-dark-pink-0, var(--p-pink-800)));
+    background: var(--p-submenu-header-bg, var(--p-dark-pink-700, var(--p-pink-0)));
+    font-weight: var(--p-submenu-header-font-weight, 700);
 }
 .p-multiselect-panel .p-multiselect-items .p-multiselect-empty-message {
-    padding: var(--p-input-list-item-padding);
-    color: var(--p-input-list-item-text-color);
-    background: var(--p-input-list-item-bg);
+    padding: var(--p-input-list-item-padding, .75rem 1.25rem);
+    color: var(--p-input-list-item-text-color, var(--p-dark-pink-0, var(--p-pink-700)));
+    background: var(--p-input-list-item-bg, var(--p-dark-pink-800, var(--p-pink-100)));
 }
 
 .p-input-filled .p-multiselect {
-    background: var(--p-input-filled-bg);
+    background: var(--p-input-filled-bg, var(--p-dark-pink-600, var(--p-pink-200)));
 }
 .p-input-filled .p-multiselect:not(.p-disabled):hover {
-    background-color: var(--p-input-filled-hover-bg);
+    background-color: var(--p-input-filled-hover-bg, var(--p-dark-input-filled-bg, var(--p-input-filled-bg)));
 }
 .p-input-filled .p-multiselect:not(.p-disabled).p-focus {
-    background-color: var(--p-input-filled-focus-bg);
+    background-color: var(--p-input-filled-focus-bg, var(--p-dark-input-filled-bg, var(--p-pink-0)));
 }
 `;
--- components/lib/theme/lara/togglebutton/index.js
+++ components/lib/theme/lara/togglebutton/index.js
@@ -1,54 +1,54 @@
 export default `
 .p-togglebutton.p-button {
-    background: var(--p-toggle-button-bg);
-    border: var(--p-toggle-button-border);
-    color: var(--p-toggle-button-text-color);
-    transition: var(--p-form-element-transition);
+    background: var(--p-toggle-button-bg, var(--p-dark-pink-800, var(--p-input-bg)));
+    border: var(--p-toggle-button-border, 1px solid var(--p-dark-pink-600, var(--p-pink-400)));
+    color: var(--p-toggle-button-text-color, var(--p-dark-pink-0, var(--p-pink-700)));
+    transition: var(--p-form-element-transition, background-color var(--p-transition-duration), color var(--p-transition-duration), border-color var(--p-transition-duration), box-shadow var(--p-transition-duration));
 }
 .p-togglebutton.p-button .p-button-icon-left,
 .p-togglebutton.p-button .p-button-icon-right {
-    color: var(--p-toggle-button-icon-color);
+    color: var(--p-toggle-button-icon-color, var(--p-dark-pink-100, var(--p-pink-600)));
 }
 .p-togglebutton.p-button:not(.p-disabled).p-focus {
-    outline: var(--p-focus-outline);
-    outline-offset: var(--p-focus-outline-offset);
+    outline: var(--p-focus-outline, 0 none);
+    outline-offset: var(--p-focus-outline-offset, 0);
     box-shadow: var(--p-focus-shadow);
-    border-color: var(--p-input-focus-border-color);
+    border-color: var(--p-input-focus-border-color, 0 0 0 .2rem var(--p-dark-primary-lightest-color, var(--p-primary-light-color)));
 }
 .p-togglebutton.p-button:not(.p-disabled):not(.p-highlight):hover {
-    background: var(--p-toggle-button-hover-bg);
-    border-color: var(--p-toggle-button-hover-border-color);
-    color: var(--p-toggle-button-text-hover-color);
+    background: var(--p-toggle-button-hover-bg, var(--p-dark-pink-900, var(--p-pink-200)));
+    border-color: var(--p-toggle-button-hover-border-color, var(--p-dark-pink-600, var(--p-pink-400)));
+    color: var(--p-toggle-button-text-hover-color, var(--p-dark-pink-0, var(--p-pink-700)));
 }
 .p-togglebutton.p-button:not(.p-disabled):not(.p-highlight):hover .p-button-icon-left,
 .p-togglebutton.p-button:not(.p-disabled):not(.p-highlight):hover .p-button-icon-right {
-    color: var(--p-toggle-button-icon-hover-color);
+    color: var(--p-toggle-button-icon-hover-color, var(--p-dark-pink-100, var(--p-pink-800)));
 }
 .p-togglebutton.p-button.p-highlight {
-    background: var(--p-toggle-button-active-bg);
-    border-color: var(--p-toggle-button-active-border-color);
-    color: var(--p-toggle-button-text-active-color);
+    background: var(--p-toggle-button-active-bg, var(--p-dark-primary-color, var(--p-primary-color)));
+    border-color: var(--p-toggle-button-active-border-color, var(--p-dark-primary-color, var(--p-primary-color)));
+    color: var(--p-toggle-button-text-active-color, var(--p-dark-primary-text-color, var(--p-primary-text-color)));
 }
 .p-togglebutton.p-button.p-highlight .p-button-icon-left,
 .p-togglebutton.p-button.p-highlight .p-button-icon-right {
-    color: var(--p-toggle-button-icon-active-color);
+    color: var(--p-toggle-button-icon-active-color, var(--p-dark-primary-text-color, var(--p-primary-text-color)));
 }
 .p-togglebutton.p-button.p-highlight:hover {
-    background: var(--p-toggle-button-active-hover-bg);
-    border-color: var(--p-toggle-button-active-hover-border-color);
-    color: var(--p-toggle-button-text-active-hover-color);
+    background: var(--p-toggle-button-active-hover-bg, var(--p-dark-primary-light-color, var(--p-primary-dark-color)));
+    border-color: var(--p-toggle-button-active-hover-border-color), var(--p-dark-primary-light-color, var(--p-primary-dark-color));
+    color: var(--p-toggle-button-text-active-hover-color, var(--p-dark-primary-text-color, var(--p-primary-text-color)));
 }
 .p-togglebutton.p-button.p-highlight:hover .p-button-icon-left,
 .p-togglebutton.p-button.p-highlight:hover .p-button-icon-right {
-    color: var(--p-toggle-button-icon-active-hover-color);
+    color: var(--p-toggle-button-icon-active-hover-color, var(--p-dark-primary-text-color, var(--p-primary-text-color)));
 }
 .p-togglebutton.p-button.p-invalid > .p-button {
-    border-color: var(--p-input-error-border-color);
+    border-color: var(--p-input-error-border-color, var(--p-dark-error-color, var(--p-error-color)));
 }
 .p-togglebutton.p-button {
     transition:
-        background-color var(--p-transition-duration),
-        border-color var(--p-transition-duration),
-        box-shadow var(--p-transition-duration);
+        background-color var(--p-transition-duration, .2s),
+        border-color var(--p-transition-duration, .2s),
+        box-shadow var(--p-transition-duration, .2s);
 }
 `;
